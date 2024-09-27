b5332d48c4e32beffbfdecc35ed02403a610d052
Javes Hernandez <javesap3@gmail.com>
2024-09-27 16:05:09 -0400

    Fix part one solution and solve part two

--- solution.js
+++ solution.js
@@ -6,8 +6,9 @@ fs.createReadStream('orders.csv')
   .pipe(csv())
   .on('data', (data) => results.push(data))
   .on('end', () => {
-
-    const partOneSolution = (ordersArr) => {
+    //console.log(results)
+    // Part 1
+    function partOneSolution(ordersArr) {
       // I created an array that contains all the customer ids and their spending
       // Incase there were multiple transactions from one account I created a conditional
       // Sadly this caused the time complexity to go up to probably n squared with the nested loops
@@ -15,12 +16,17 @@ fs.createReadStream('orders.csv')
       const customerSpending = []
 
       for (const order of ordersArr) {
-        if (!customerSpending.forEach((customer) => customer.customerId === order.customer_id)) {
+        let isIncluded = false
+        customerSpending.forEach((customer) => {
+          if (customer.customerId === order.customer_id) isIncluded = true
+        })
+
+        if (!isIncluded) {
           customerSpending.push({customerId: order.customer_id, totalSpent: order.price_per_unit})
         } else {
           customerSpending.forEach((customer) => {
             if (customer.customerId === order.customer_id) {
-              customer.totalSpent + order.price_per_unit
+              customer.totalSpent = customer.totalSpent + Number(order.price_per_unit)
             }
           })
         }
@@ -29,5 +35,54 @@ fs.createReadStream('orders.csv')
       const topFiveCustomers = customerSpending.sort((a, b) => b.totalSpent - a.totalSpent).slice(0,5)
       console.log(topFiveCustomers)
     }
-    partOneSolution(results)
+    
+    // Part 2
+    // Both functions are very similar and could be abstracted into one
+    // Both functions have a time complexity of n^2 since they have two nested loops 
+    // and these loops rely on the input meaning the bigger the input the more operations 
+    // Their space complexity stays constant as no more variables are created depending on the input
+    function mostCustomerOrders(ordersArr) {
+      let customerOrders = []
+      ordersArr.forEach((order) => {
+        let isIncluded = false
+        customerOrders.forEach((customer) => {
+          if (customer.customerId === order.customer_id) isIncluded = true
+        })
+        if (isIncluded) {
+          customerOrders.forEach((customer) => {
+            if (customer.customerId === order.customer_id) {
+              customer.orders += 1
+            }
+          })
+        } else {
+          customerOrders.push({customerId: order.customer_id, orders: 1})
+        }
+      })
+      const CustomersWithMostOrders = customerOrders.sort((a,b) => b.orders - a.orders).slice(0,1)
+      console.log(CustomersWithMostOrders)
+    }
+
+    function mostOrderedProduct(ordersArr) {
+      const productOrders = []
+      for (const order of ordersArr) {
+        let isIncluded = false
+        productOrders.forEach((product) => {
+          if (product.productId === order.product_id) isIncluded = true
+        })
+
+        if (!isIncluded) {
+          productOrders.push({productId: order.product_id, orders: order.quantity})
+        } else {
+          productOrders.forEach((product) => {
+            if (product.productId === order.product_id) {
+              product.orders = product.orders + order.product_id
+            }
+          })
+        }
+      }
+      const mostPopularProduct = productOrders.sort((a,b) => b.orders - a.orders).slice(0,1)
+      console.log(mostPopularProduct)
+    }
+
+    mostOrderedProduct(results)
   });
